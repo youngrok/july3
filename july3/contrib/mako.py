fa6f7c7c1318a37f1a6727eb748a0b0bec37a0df
Pak Youngrok <pak.youngrok@gmail.com>
2016-05-04 12:11:47 +0900

    text temp file

--- july3/contrib/mako.py
+++ july3/contrib/mako.py
@@ -5,7 +5,7 @@ from july3.util import run
 
 
 def render_template(source, target, context, sudo=False):
-    with NamedTemporaryFile() as tmp:
+    with NamedTemporaryFile('w') as tmp:
         tmp.file.write(Template(filename=source).render(**context))
         tmp.file.flush()
         sudo_cmd = 'sudo ' if sudo else ''
