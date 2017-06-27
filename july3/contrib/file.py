f0e9d6f2e4536c7ebbdeef399dfe561f2d985dcd
Pak Youngrok <pak.youngrok@gmail.com>
2017-06-28 01:49:38 +0900

    basic rules

---
+++ july3/contrib/file.py
@@ -0,0 +1,38 @@
+import os
+
+from july3 import env
+from july3.rule import Rule
+from july3.util import render_percent_template, run
+
+
+class TemplateRendered(Rule):
+    def __init__(self, target, source, render_fn=render_percent_template, sudo=False, dependencies=None):
+        self.source = source
+        self.render_fn = render_fn
+        self.sudo = sudo
+        super().__init__(target, dependencies=[source] + (dependencies or []))
+
+    @staticmethod
+    def command(self):
+        self.render_fn(self.source, self.target, env, sudo=self.sudo)
+
+
+class Directory(Rule):
+    def __init__(self, target, sudo=False, dependencies=None):
+        self.sudo = sudo
+        super().__init__(target, dependencies=dependencies)
+
+    @staticmethod
+    def command(self):
+        if not os.path.exists(self.target):
+            os.makedirs(self.target)
+
+
+class CommandRule(Rule):
+    def __init__(self, command, sudo=False, dependencies=None):
+        self.sudo = sudo
+        super().__init__(command, dependencies=dependencies)
+
+    def is_made(self):
+        print('bower command exists:', run(f'type {self.command}').returncode)
+        return run(f'type {self.command}').returncode == 0
